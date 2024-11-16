e84f867a4ad80ad7eaab97a8d8861e77dfc8e09b
Vmiu <miuvictor21079@gmail.com>
2024-11-16 17:47:23 +0800

    clear unuse file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { ClerkProvider, SignInButton, SignedIn, SignedOut} from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: "COMP4651FP",
@@ -27,17 +15,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body>
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
-            <UserButton />
             <main>{children}</main>
           </SignedIn>
-
         </body>
       </html>
     </ClerkProvider>
